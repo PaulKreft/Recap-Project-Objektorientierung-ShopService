6d1956bc51c1e070b19c5974beea3e44d738ff88
paulkreft <paulykreft@gmail.com>
2023-12-18 17:33:41 +0100

    add ZonedTimeStamp to Order and adjust constructor

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,14 +1,17 @@
 import lombok.With;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @With
 public record Order(
         String id,
         List<Product> products,
-        OrderStatus status
+        OrderStatus status,
+        ZonedDateTime timestamp
 ) {
-    public Order(String id, List<Product> products) {
-        this(id, products, OrderStatus.PROCESSING);
+
+    public Order(String id, List<Product> products, ZonedDateTime timestamp) {
+        this(id, products, OrderStatus.PROCESSING, timestamp);
     }
 }
