16fe96ec5491a37524ceec4378dea9c454f9f9a8
paulkreft <paulykreft@gmail.com>
2023-12-18 17:15:48 +0100

    remove Main contents

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,5 @@ import java.util.UUID;
 public class Main {
 
     public static void main(String[] args) {
-        List<Product> productsToBeOrdered = new ArrayList<>();
-
-        productsToBeOrdered.add(new Product(UUID.randomUUID().toString(), "Kaki"));
-        Order order = new Order(UUID.randomUUID().toString(), productsToBeOrdered, OrderStatus.PROCESSING);
     }
 }
